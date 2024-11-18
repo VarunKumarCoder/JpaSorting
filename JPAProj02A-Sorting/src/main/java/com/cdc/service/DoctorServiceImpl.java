e5f06ef1237e20358f05fd8ae13ba7e40f06abd1
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-18 14:12:22 +0530

    service method definition completed

--- JPAProj02A-Sorting/src/main/java/com/cdc/service/DoctorServiceImpl.java
+++ JPAProj02A-Sorting/src/main/java/com/cdc/service/DoctorServiceImpl.java
@@ -1,13 +1,21 @@
 package com.cdc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
 import com.cdc.entity.Doctor;
+import com.cdc.repository.IDoctorRepository;
 
 public class DoctorServiceImpl implements IDoctorService {
+	@Autowired
+	private IDoctorRepository doctorRepo;
 
 	@Override
 	public Iterable<Doctor> showDoctorsBySorting(boolean asc, String... props) {
-		// TODO Auto-generated method stub
-		return null;
+		Sort sort = Sort.by(asc ? Direction.ASC : Direction.DESC, props);
+		Iterable<Doctor> it = doctorRepo.findAll(sort);
+		return it;
 	}
 
 }
