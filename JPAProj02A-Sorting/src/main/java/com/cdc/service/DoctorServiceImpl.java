1d82aba46a976125acfbaa2929529e0189209288
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-18 14:06:45 +0530

    Service Impl class created and methods overridden

---
+++ JPAProj02A-Sorting/src/main/java/com/cdc/service/DoctorServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cdc.service;
+
+import com.cdc.entity.Doctor;
+
+public class DoctorServiceImpl implements IDoctorService {
+
+	@Override
+	public Iterable<Doctor> showDoctorsBySorting(boolean asc, String... props) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
