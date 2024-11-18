72f6d50768ed0affc5880a403e6d8b84c6f8183b
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-18 14:23:09 +0530

    Runner class created

--- JPAProj02A-Sorting/src/main/java/com/cdc/runner/SortTestRunner.java
+++ JPAProj02A-Sorting/src/main/java/com/cdc/runner/SortTestRunner.java
@@ -1,14 +1,21 @@
 package com.cdc.runner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.cdc.service.IDoctorService;
+
 @Component
 public class SortTestRunner implements CommandLineRunner {
+	@Autowired
+	private IDoctorService service;
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		service.showDoctorsBySorting(true, "docName").forEach(System.out::println);
+		System.out.println("________________________________");
+		service.showDoctorsBySorting(false, "docName").forEach(System.out::println);
 	}
 
 }
